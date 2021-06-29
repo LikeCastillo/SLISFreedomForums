4880c353434cf9b3c6f15e7d88d10fa328add9ae
LikeCastillo <63914050+LikeCastillo@users.noreply.github.com>
2021-06-30 01:39:20 +0800

    Added Files via Upload
    Added Functionality to CRUD Posts, including only giving access to update and delete the posts to authors
    Added an Update Account Functionality
    Added a Profile Picture Functionality

--- app/models.py
+++ app/models.py
@@ -27,7 +27,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(90), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
+    content = db.Column(db.Text(500), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
--- app/routes.py
+++ app/routes.py
@@ -1,25 +1,12 @@
-from flask import render_template, request, redirect, url_for, session, flash
+import os
+import secrets
+from PIL import Image
+from flask import render_template, request, redirect, url_for, session, flash, abort
 from app import app, db, encode
-from app.forms import RegistrationForm, LoginForm
+from app.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from app.models import User,Post
 from flask_login import login_user, current_user, logout_user, login_required
 
-posts = [
-    {
-        'author' : 'Jan Francis',
-        'username' : 'LikeCastillo',
-        'title' : 'My First SLIS Post',
-        'content' : 'Hello!',
-        'date_posted': 'June 27, 2021'
-    },
-    {
-        'author' : 'Jiez and Coral',
-        'username' : 'CoralAndJiez',
-        'title' : 'My Second SLIS Post',
-        'content' : 'Goodbye!',
-        'date_posted': 'June 27, 2021'
-    }
-]
 
 @app.route('/')
 @app.route('/home')
@@ -27,6 +14,7 @@ def home():
     # if 'username' in session:
     #     return f'Logged in as {session["username"]}'
     # return 'You are not logged in'
+    posts = Post.query.all()
     return render_template('home.html', posts=posts)
 
 
@@ -77,10 +65,96 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+#hi jf these are the stuff i added last night hehehe
 
-@app.route("/account")
+#uploading new profile pic
+#Pictures uploaded will be stored in the static folder
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, "static/profile_pics", picture_fn)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+
+#updating account info
+@app.route("/account", methods=["GET", "POST"])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.prof_pic = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash("Your account has been updated.", "success")
+        return redirect(url_for("account"))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+    prof_pic = url_for('static', filename='profile_pics/' + current_user.prof_pic)
+    return render_template('account.html', title='Account', prof_pic=prof_pic, form=form)
+
+
+#new post
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post',
+                           form=form, legend='New Post')
+
+#if post url doesn't exist
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template("post.html", title=post.title, post=post)
+
+
+#udpate post
+@app.route("/post/<int:post_id>/update", methods=["GET", "POST"])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash("Your post has been updated.", "success")
+        return redirect(url_for("post", post_id=post.id))
+    elif request.method == "GET":
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post', form=form, legend='UpdatePost')
+
+# delete post
+@app.route("/post/<int:post_id>/delete", methods=["POST"])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash("Your post has been deleted.", "success")
+    return redirect(url_for("home"))
 
 # <a class="nav-item nav-link" href="/register">Register</a>
