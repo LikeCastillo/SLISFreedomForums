ff850e4fa04e97d3328965c539fb3c6acc07f9d7
LikeCastillo <63914050+LikeCastillo@users.noreply.github.com>
2021-06-30 03:56:29 +0800

    Add files via upload

---
+++ app/forms.py
@@ -0,0 +1,74 @@
+# This Python File will mainly be used for collecting info upon registering in the Forums.
+
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms_validators import DisposableEmail
+from app.models import User
+
+
+
+#Registration Form Class, Labeling out Form Fields Needed to be Filled Out
+class RegistrationForm(FlaskForm):
+    #attributes
+    username = StringField('Username',
+                           # validators - parameters to accept user input
+                           validators=[DataRequired(), Length(min=2, max=25)])
+    email = StringField('Email',
+                        validators=[DataRequired(), DisposableEmail()])
+    password = PasswordField('Password', validators=[DataRequired()])
+
+    confirm = PasswordField('Confirm Password',
+                            validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    #Validating User Input in Registration
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username Exists Already!')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email Exists Already!')
+
+
+class LoginForm(FlaskForm):
+    #For logging in
+    username = StringField('Username',
+                           # validators - parameters to accept user input
+                           validators=[DataRequired(), Length(min=2, max=25)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In!')
+
+class UpdateAccountForm(FlaskForm):
+    #attributes
+    username = StringField('Username',
+                           # validators - parameters to accept user input
+                           validators=[DataRequired(), Length(min=2, max=25)])
+    email = StringField('Enter your Email here.',
+                        validators=[DataRequired(), DisposableEmail()])
+    picture = FileField("Update Profile Picture", validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    #Validating User Input in Registration
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('Username Exists Already!')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('Email Exists Already!')
+
+class PostForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired()])
+    content = TextAreaField("Content", validators=[DataRequired(), Length(max=750)])
+    submit = SubmitField("Post")
---
+++ app/models.py
@@ -0,0 +1,34 @@
+from datetime import  datetime
+from app import db, loginm
+#UserMixin adds all methods and attributes needed for the User Model
+from flask_login import UserMixin
+
+#takes user id as argument (for logging in)
+@loginm.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+#Unique ID for each user/entry in the user database
+#User Info Class
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    prof_pic = db.Column(db.String(20), nullable=False, default='bingus.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    #shows relationship of posts made to a single author that wrote them, backref is adding pseudo column to post model
+    posts = db.relationship('Post', backref='author', lazy=True)
+    #How our user object (info) will be printed
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.prof_pic}')"
+
+#Class for posts made in the Freedom Forums
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(90), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text(500), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
